ff3190184b5ed528e48a802665ac2410728d203b
Ornexanet <ornexawork@gmail.com>
2026-05-29 21:14:58 +0200

    Fix category links

--- src/components/Home/Categories/categoryData.ts
+++ src/components/Home/Categories/categoryData.ts
@@ -1,49 +1,49 @@
 const data = [
   {
     title: "Televisions",
-    href: "/shop",
+    href: "/shop-with-sidebar"
     id: 1,
     img: "/images/categories/categories-01.png",
   },
   {
     title: "Laptop & PC",
-    href: "/shop",
+    href: "/shop-with-sidebar"
     id: 2,
     img: "/images/categories/categories-02.png",
   },
   {
     title: "Mobile & Tablets",
-    href: "/shop",
+    href: "/shop-with-sidebar"
     id: 3,
     img: "/images/categories/categories-03.png",
   },
   {
     title: "Games & Videos",
-    href: "/shop",
+    href: "/shop-with-sidebar"
     id: 4,
     img: "/images/categories/categories-04.png",
   },
   {
     title: "Home Appliances",
-    href: "/shop",
+    href: "/shop-with-sidebar"
     id: 5,
     img: "/images/categories/categories-05.png",
   },
   {
     title: "Health & Sports",
-    href: "/shop",
+    href: "/shop-with-sidebar"
     id: 6,
     img: "/images/categories/categories-06.png",
   },
   {
     title: "Watches",
-    href: "/shop",
+    href: "/shop-with-sidebar"
     id: 7,
     img: "/images/categories/categories-07.png",
   },
   {
     title: "Televisions",
-    href: "/shop",
+    href: "/shop-with-sidebar"
     id: 8,
     img: "/images/categories/categories-04.png",
   },
