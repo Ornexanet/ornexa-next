e0def8b0421c715c23c27d558eb34a362fc844f0
Ornexanet <ornexawork@gmail.com>
2026-05-29 15:29:50 +0200

    translate header menu to Swedish and fix category item build

--- nextjs-ecommerce-template-main/src/components/Header/menuData.ts
+++ nextjs-ecommerce-template-main/src/components/Header/menuData.ts
@@ -3,25 +3,25 @@ import { Menu } from "@/types/Menu";
 export const menuData: Menu[] = [
   {
     id: 1,
-    title: "Popular",
+    title: "Populärt",
     newTab: false,
     path: "/",
   },
   {
     id: 2,
-    title: "Shop",
+    title: "Butik",
     newTab: false,
     path: "/shop-with-sidebar",
   },
   {
     id: 3,
-    title: "Contact",
+    title: "Kontakt",
     newTab: false,
     path: "/contact",
   },
   {
     id: 6,
-    title: "pages",
+    title: "Sidor",
     newTab: false,
     path: "/",
     submenu: [
@@ -95,7 +95,7 @@ export const menuData: Menu[] = [
   },
   {
     id: 7,
-    title: "blogs",
+    title: "Blogg",
     newTab: false,
     path: "/",
     submenu: [
