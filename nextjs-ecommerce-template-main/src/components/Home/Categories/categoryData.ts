dd75371001845a7c196239661b343ce6b17af46b
Ornexanet <ornexawork@gmail.com>
2026-05-29 20:26:12 +0200

    Fix televisions category link

--- nextjs-ecommerce-template-main/src/components/Home/Categories/categoryData.ts
+++ nextjs-ecommerce-template-main/src/components/Home/Categories/categoryData.ts
@@ -1,7 +1,7 @@
 const data = [
   {
     title: "Televisions",
-    href: "/category/televisions",
+    href: "/shop-with-sidebar",
     id: 1,
     img: "/images/categories/categories-01.png",
   },
