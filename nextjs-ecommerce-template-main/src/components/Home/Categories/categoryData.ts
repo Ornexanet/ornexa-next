905d35ab46dd4657da038fccde5db45f78ebbac1
Ornexanet <ornexawork@gmail.com>
2026-05-29 20:49:11 +0200

    Fix category links

--- nextjs-ecommerce-template-main/src/components/Home/Categories/categoryData.ts
+++ nextjs-ecommerce-template-main/src/components/Home/Categories/categoryData.ts
@@ -7,36 +7,43 @@ const data = [
   },
   {
     title: "Laptop & PC",
+    href: "/shop-with-sidebar",
     id: 2,
     img: "/images/categories/categories-02.png",
   },
   {
     title: "Mobile & Tablets",
+    href: "/shop-with-sidebar",
     id: 3,
     img: "/images/categories/categories-03.png",
   },
   {
     title: "Games & Videos",
+    href: "/shop-with-sidebar",
     id: 4,
     img: "/images/categories/categories-04.png",
   },
   {
     title: "Home Appliances",
+    href: "/shop-with-sidebar",
     id: 5,
     img: "/images/categories/categories-05.png",
   },
   {
     title: "Health & Sports",
+    href: "/shop-with-sidebar",
     id: 6,
     img: "/images/categories/categories-06.png",
   },
   {
     title: "Watches",
+    href: "/shop-with-sidebar",
     id: 7,
     img: "/images/categories/categories-07.png",
   },
   {
     title: "Televisions",
+    href: "/shop-with-sidebar",
     id: 8,
     img: "/images/categories/categories-04.png",
   },
