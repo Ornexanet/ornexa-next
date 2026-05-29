e652fae69b695a18f9f076ce4661d487d4940707
Ornexanet <ornexawork@gmail.com>
2026-05-29 11:42:40 +0200

    Add href to all category data

--- src/components/Home/Categories/categoryData.ts
+++ src/components/Home/Categories/categoryData.ts
@@ -7,36 +7,43 @@ const data = [
   },
   {
     title: "Laptop & PC",
+    href: "/shop",
     id: 2,
     img: "/images/categories/categories-02.png",
   },
   {
     title: "Mobile & Tablets",
+    href: "/shop",
     id: 3,
     img: "/images/categories/categories-03.png",
   },
   {
     title: "Games & Videos",
+    href: "/shop",
     id: 4,
     img: "/images/categories/categories-04.png",
   },
   {
     title: "Home Appliances",
+    href: "/shop",
     id: 5,
     img: "/images/categories/categories-05.png",
   },
   {
     title: "Health & Sports",
+    href: "/shop",
     id: 6,
     img: "/images/categories/categories-06.png",
   },
   {
     title: "Watches",
+    href: "/shop",
     id: 7,
     img: "/images/categories/categories-07.png",
   },
   {
     title: "Televisions",
+    href: "/shop",
     id: 8,
     img: "/images/categories/categories-04.png",
   },
