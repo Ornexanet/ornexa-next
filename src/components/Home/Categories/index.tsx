728b7b4c78b4b86dc9832eb93340987ec052480f
Ornexanet <ornexawork@gmail.com>
2026-05-30 12:23:40 +0200

    Translate category section title

--- src/components/Home/Categories/index.tsx
+++ src/components/Home/Categories/index.tsx
@@ -70,10 +70,10 @@ const Categories = () => {
                     </clipPath>
                   </defs>
                 </svg>
-                Categories
+                Kategorier
               </span>
               <h2 className="font-semibold text-xl xl:text-heading-5 text-dark">
-                Browse by Category
+                Bläddra efter Kategorier
               </h2>
             </div>
 
