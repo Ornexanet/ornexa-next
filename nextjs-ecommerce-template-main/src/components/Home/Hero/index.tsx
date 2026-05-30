e89b06853888c67f7eb2fa62ff592c2b3f51153c
Ornexanet <ornexawork@gmail.com>
2026-05-30 11:35:27 +0200

    Update hero cards text and links

--- nextjs-ecommerce-template-main/src/components/Home/Hero/index.tsx
+++ nextjs-ecommerce-template-main/src/components/Home/Hero/index.tsx
@@ -29,19 +29,19 @@ const Hero = () => {
                 <div className="flex items-center gap-14">
                   <div>
                     <h2 className="max-w-[153px] font-semibold text-dark text-xl mb-20">
-                      <a href="#"> iPhone 14 Plus & 14 Pro Max </a>
+                      <a href="/shop-with-sidebar"> iPhone 14 Plus & 14 Pro Max </a>
                     </h2>
 
                     <div>
                       <p className="font-medium text-dark-4 text-custom-sm mb-1.5">
-                        limited time offer
+                        Tidsbegränsat erbjudande 
                       </p>
                       <span className="flex items-center gap-3">
                         <span className="font-medium text-heading-5 text-red">
-                          $699
+                          7499 kr
                         </span>
                         <span className="font-medium text-2xl text-dark-4 line-through">
-                          $999
+                          9999 kr
                         </span>
                       </span>
                     </div>
@@ -61,19 +61,19 @@ const Hero = () => {
                 <div className="flex items-center gap-14">
                   <div>
                     <h2 className="max-w-[153px] font-semibold text-dark text-xl mb-20">
-                      <a href="#"> Wireless Headphone </a>
+                      <a href="/shop-with-sidebar">Trådlösa Hörlurar</a>
                     </h2>
 
                     <div>
                       <p className="font-medium text-dark-4 text-custom-sm mb-1.5">
-                        limited time offer
+                        Tidsbegränsat erbjudande 
                       </p>
                       <span className="flex items-center gap-3">
                         <span className="font-medium text-heading-5 text-red">
-                          $699
+                          1499 kr
                         </span>
                         <span className="font-medium text-2xl text-dark-4 line-through">
-                          $999
+                          2199 kr
                         </span>
                       </span>
                     </div>
