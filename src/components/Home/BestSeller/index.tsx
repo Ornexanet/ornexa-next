507d30753a6deaa2f8de100a260f3c6039f58da7
Ornexanet <ornexawork@gmail.com>
2026-05-31 22:47:26 +0200

    Translate Best Seller to Swedish

--- src/components/Home/BestSeller/index.tsx
+++ src/components/Home/BestSeller/index.tsx
@@ -27,7 +27,7 @@ const BestSeller = () => {
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7.5">
-          {/* <!-- Best Sellers item --> */}
+          {/* <!-- Bästsäljare item --> */}
           {shopData.slice(1, 7).map((item, key) => (
             <SingleItem item={item} key={key} />
           ))}
