c3432bfb24947b11269935568b5e95a3e84d778e
Ornexanet <ornexawork@gmail.com>
2026-06-01 10:23:54 +0200

    Translate countdown banner to Swedish

--- src/components/Home/Countdown/index.tsx
+++ src/components/Home/Countdown/index.tsx
@@ -32,14 +32,14 @@ const CounDown = () => {
         <div className="relative overflow-hidden z-1 rounded-lg bg-[#D0E9F3] p-4 sm:p-7.5 lg:p-10 xl:p-15">
           <div className="max-w-[422px] w-full">
             <span className="block font-medium text-custom-1 text-blue mb-2.5">
-              Don’t Miss!!
+              Missa inte!!
             </span>
 
             <h2 className="font-bold text-dark text-xl lg:text-heading-4 xl:text-heading-3 mb-3">
-              Enhance Your Music Experience
+              Förbättra din musikupplevelse
             </h2>
 
-            <p>The Havit H206d is a wired PC headphone.</p>
+            <p>Havit H206d är ett trådbundet PC-headset.</p>
 
             {/* <!-- Countdown timer --> */}
             <div
@@ -57,7 +57,7 @@ const CounDown = () => {
                   {days < 10 ? "0" + days : days}{" "}
                 </span>
                 <span className="block text-custom-sm text-dark text-center">
-                  Days
+                  Dagar
                 </span>
               </div>
 
@@ -71,7 +71,7 @@ const CounDown = () => {
                   {hours < 10 ? "0" + hours : hours}{" "}
                 </span>
                 <span className="block text-custom-sm text-dark text-center">
-                  Hours
+                  Timmar
                 </span>
               </div>
 
@@ -84,7 +84,7 @@ const CounDown = () => {
                   {minutes < 10 ? "0" + minutes : minutes}{" "}
                 </span>
                 <span className="block text-custom-sm text-dark text-center">
-                  Minutes
+                  Minuter
                 </span>
               </div>
 
@@ -97,17 +97,17 @@ const CounDown = () => {
                   {seconds < 10 ? "0" + seconds : seconds}{" "}
                 </span>
                 <span className="block text-custom-sm text-dark text-center">
-                  Seconds
+                  Sekunder
                 </span>
               </div>
             </div>
             {/* <!-- Countdown timer ends --> */}
 
             <a
-              href="#"
+              href="/shop-with-sidebar"
               className="inline-flex font-medium text-custom-sm text-white bg-blue py-3 px-9.5 rounded-md ease-out duration-200 hover:bg-blue-dark mt-7.5"
             >
-              Check it Out!
+              Se erbjudandet!
             </a>
           </div>
 
