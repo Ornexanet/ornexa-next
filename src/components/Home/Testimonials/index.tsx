a179352810580ecd93b4ad709af1420b68937928
Ornexanet <ornexawork@gmail.com>
2026-06-01 10:48:55 +0200

    Translate User Feedbacks to Swedish

--- src/components/Home/Testimonials/index.tsx
+++ src/components/Home/Testimonials/index.tsx
@@ -37,10 +37,10 @@ const Testimonials = () => {
                     width={17}
                     height={17}
                   />
-                  Testimonials
+                  Recensioner
                 </span>
                 <h2 className="font-semibold text-xl xl:text-heading-5 text-dark">
-                  User Feedbacks
+                Kundomdömen
                 </h2>
               </div>
 
