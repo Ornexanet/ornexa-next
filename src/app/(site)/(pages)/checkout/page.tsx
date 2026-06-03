4c680cbaad91cde7ccb88ca5e150bb082e299dcb
Ornexanet <ornexawork@gmail.com>
2026-06-03 21:13:24 +0200

    Optimize checkout metadata

--- src/app/(site)/(pages)/checkout/page.tsx
+++ src/app/(site)/(pages)/checkout/page.tsx
@@ -3,8 +3,8 @@ import Checkout from "@/components/Checkout";
 
 import { Metadata } from "next";
 export const metadata: Metadata = {
-  title: "Checkout Page | NextCommerce Nextjs E-commerce template",
-  description: "This is Checkout Page for NextCommerce Template",
+  title: "Kassa | Slutför ditt köp hos Ornexa",
+  description: "Slutför din beställning hos Ornexa på ett enkelt och smidigt sätt. Granska dina produkter, fyll i dina uppgifter och gå vidare till betalning.",
   // other metadata
 };
 
