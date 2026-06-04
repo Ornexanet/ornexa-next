798427c0d4cef7d856ba0b9b2a3912ad312178f3
Ornexanet <ornexawork@gmail.com>
2026-06-04 10:32:39 +0200

    Update metadata for shop without sidebar page

--- src/app/(site)/(pages)/shop-without-sidebar/page.tsx
+++ src/app/(site)/(pages)/shop-without-sidebar/page.tsx
@@ -3,8 +3,8 @@ import ShopWithoutSidebar from "@/components/ShopWithoutSidebar";
 
 import { Metadata } from "next";
 export const metadata: Metadata = {
-  title: "Shop Page | NextCommerce Nextjs E-commerce template",
-  description: "This is Shop Page for NextCommerce Template",
+  title: "Alla produkter | Ornexa",
+  description: "Utforska alla produkter hos Ornexa. Bläddra bland kategorier, priser och utvalda erbjudanden i en enkel butiksvy.",
   // other metadata
 };
 
