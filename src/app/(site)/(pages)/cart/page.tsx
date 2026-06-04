7da76e86b4928f944742f110f605e0fb42b9b070
Ornexanet <ornexawork@gmail.com>
2026-06-04 10:57:02 +0200

    Update metadata for cart page

--- src/app/(site)/(pages)/cart/page.tsx
+++ src/app/(site)/(pages)/cart/page.tsx
@@ -3,8 +3,8 @@ import Cart from "@/components/Cart";
 
 import { Metadata } from "next";
 export const metadata: Metadata = {
-  title: "Cart Page | NextCommerce Nextjs E-commerce template",
-  description: "This is Cart Page for NextCommerce Template",
+  title: "Varukorg | Ornexa",
+  description: "Se produkterna i din varukorg, kontrollera priser och fortsätt till kassan hos Ornexa.",
   // other metadata
 };
 
