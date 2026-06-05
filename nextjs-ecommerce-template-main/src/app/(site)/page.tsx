6f8c8abb5140a2c946f5021502d58340030339bc
Ornexanet <ornexawork@gmail.com>
2026-06-05 19:15:26 +0200

    Fix hompage canonical metadata

--- nextjs-ecommerce-template-main/src/app/(site)/page.tsx
+++ nextjs-ecommerce-template-main/src/app/(site)/page.tsx
@@ -1,5 +1,5 @@
 import Home from "@/components/Home";
-import { Metadata } from "next";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Ornexa | Elektronik, Tillbehör & Smart Shopping",
