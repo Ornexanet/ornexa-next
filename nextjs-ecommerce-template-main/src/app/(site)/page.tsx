84b00a23b521f67e98827f6b24684e4e84b5cdd0
Ornexanet <ornexawork@gmail.com>
2026-06-05 18:58:29 +0200

    Add canonical to homepage

--- nextjs-ecommerce-template-main/src/app/(site)/page.tsx
+++ nextjs-ecommerce-template-main/src/app/(site)/page.tsx
@@ -4,6 +4,9 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Ornexa | Elektronik, Tillbehör & Smart Shopping",
   description: "Upptäck elektronik, tillbehör och smarta erbjudanden hos Ornexa. Handla kvalitetsprodukter med en modern och smidig shoppingupplevelse.",
+  alternates: {
+    canonical: "https//lab.ornexa.net/",
+  },
   // other metadata
 };
 
