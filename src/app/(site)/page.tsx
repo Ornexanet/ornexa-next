27236a01ae8551b5d6e9bcb8a0af9dc14c739eb5
Ornexanet <ornexawork@gmail.com>
2026-06-05 19:35:02 +0200

    Fix canonical for homepage

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -4,6 +4,10 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Ornexa | Elektronik, Tillbehör & Smart Shopping",
   description: "Upptäck elektronik, tillbehör och smarta erbjudanden hos Ornexa. Handla kvalitetsprodukter med en modern och smidig shoppingupplevelse.",
+  abstract: {
+  canonical: "https://lab.ornexa.net/",
+  },
+
   // other metadata
 };
 
